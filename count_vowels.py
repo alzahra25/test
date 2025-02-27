78255903c47e6158d632bd8226220a282f83b57f
alzahra25 <alzahra.alhdhrami@gmail.com>
2025-02-27 02:12:40 -0800

    EDIT

--- count_vowels.py
+++ count_vowels.py
@@ -1,13 +1,10 @@
 user = input("Enter a string: ")
-
 if user:
     count = 0
     vowels = "aeiouAEIOU"
-    
     for char in user:
         if char in vowels:
             count += 1
-    
     print(f"Number of vowels: {count}")
 else:
     print("Try again")
