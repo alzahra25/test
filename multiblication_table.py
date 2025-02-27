35c7700555635b31d8d7e45ce1a74b15e0388f18
Tamadhir10 <tamadhir.alhabsi@gmail.com>
2025-02-27 14:05:47 +0400

    adding multiblication table

---
+++ multiblication_table.py
@@ -0,0 +1,33 @@
+print("Multiplication Table")
+
+def generate_multiplication_table():
+    number = int(input("Enter the number for the multiplication table: "))
+    print(f"\nMultiplication Table for {number} (from 1 to 10):")
+    i = 1
+    while i <= 10:
+        print(f"{number} x {i} = {number * i}")
+        i += 1  
+generate_multiplication_table()
+print("Thank you")
+
+print("Task2")
+
+num = int(input("Enter the number: "))
+r = int(input("Enter the Range: "))
+
+for n in range(r):
+    for nn in range(n+1):
+        print(nn +1, end="")
+    print("")  
+
+
+for n in range(r):
+    print("*" * (1+n))
+
+
+for n in range(r):
+  
+    print(" " * (4 - n), end="")
+    print("*" * (2 * n + 1))
+
+        
